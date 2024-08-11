f9b4d4e128d477eceba448d7cb2a744de8840571
emonkhan500 <khdilo47@gmail.com>
2024-08-11 12:55:01 +0600

    banner complete

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,48 +3,44 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-          <div className="carousel w-full">
+          <div className="carousel fonts w-full">
   <div id="slide1" className="carousel-item relative w-full">
-  <div className='flex w-full gap-20 px-20 bg-black justify-center items-center '> 
-  <h1 className='text-6xl  '>YOUR <br /> <span>FIRST HIT</span><br /> STARTS 
+  <div className='flex w-full  justify-between px-16   lg:px-80 bg-black items-center '> 
+  <div>
+  <h1 className='text-6xl flex-1 font-extrabold text-white '>YOUR <br />       <span className='text-[#F48C06] font-extrabold'>FIRST HIT</span> <br />
+   STARTS 
   <br />HERE</h1> 
+  <h1 className='text-white text-base mt-5'>What’s trending right now :  <span className='text-[#FAA307] text-xs'>#lovely Billie Eilish, Khalid </span></h1>
+  </div>
   
    <img
       src="https://i.ibb.co/ZLb1tfp/bg.png"
-      className="h-96" />
+      className= " h-[420px]" />
       </div>
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide4" className="text-3xl text-white ">❮</a>
+      <a href="#slide2" className="text-3xl text-white ">❮</a>
       <a href="#slide2" className=" text-3xl text-white ">❯</a>
     </div>
   </div>
   <div id="slide2" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-      className="w-full" />
+  <div className='flex w-full  justify-between px-16   lg:px-80 bg-black items-center '> 
+  <div>
+  <h1 className='text-6xl flex-1 font-extrabold text-white '>YOUR <br />       <span className='text-[#F48C06] font-extrabold'>FIRST HIT</span> <br />
+   STARTS 
+  <br />HERE</h1> 
+  <h1 className='text-white text-base mt-5'>What’s trending right now :  <span className='text-[#FAA307] text-xs'>#bad guy Billie Eilish </span></h1>
+  </div>
+  
+   <img
+      src="https://i.ibb.co/ZLb1tfp/bg.png"
+      className= " h-[420px]" />
+      </div>
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide1" className="btn btn-circle">❮</a>
-      <a href="#slide3" className="btn btn-circle">❯</a>
+      <a href="#slide1" className="text-3xl text-white ">❮</a>
+      <a href="#slide1" className=" text-3xl text-white ">❯</a>
     </div>
   </div>
-  <div id="slide3" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-      className="w-full" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide2" className="btn btn-circle">❮</a>
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-  <div id="slide4" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-      className="w-full" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide3" className="btn btn-circle">❮</a>
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
+ 
 </div>
           
         </div>

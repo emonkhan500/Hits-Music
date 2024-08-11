917d8a971b84cf0a9d304037cfc1754136d30426
emonkhan500 <khdilo47@gmail.com>
2024-08-11 13:51:24 +0600

    release complete

---
+++ src/Components/Release/Release.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const Release = () => {
+    return (
+        <div className='bg-black pt-20 pb-10 fonts'>
+            <h1 className='text-center text-white text-3xl font-normal tracking-widest pb-6'>LATEST RELEASES</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-40 lg:px-72'>
+
+            <div className="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
+	<img src="https://i.ibb.co/nbYgnV3/Rectangle.png" alt="" className="object-cover object-center w-80  h-80 dark:bg-gray-500" />
+	<div className="flex flex-col justify-between py-6 space-y-8">
+		<div className="space-y-2 text-white">
+            <p className='text-xl tracking-wider'>Nella feat. Tommy</p>
+			<h2 className="text-3xl font-bold tracking-wide">Mattea Dega</h2>
+            <p><span className='text-[#F48C06]'>#techno</span> drivingpeak time</p>
+			
+		</div>
+		
+	</div>
+</div>
+            <div className="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
+	<img src="https://i.ibb.co/4tRq1tv/Rectangle-1.png" alt="" className="object-cover object-center w-80  h-80 dark:bg-gray-500" />
+	<div className="flex flex-col justify-between py-6 space-y-8">
+		<div className="space-y-2 text-white">
+            <p className='text-xl tracking-wider'>Sunday Mix</p>
+			<h2 className="text-3xl font-bold tracking-wide">Nat dega</h2>
+            <p><span className='text-[#F48C06]'>#techno</span> drivingpeak time</p>
+			
+		</div>
+		
+	</div>
+</div>
+            <div className="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
+	<img src="https://i.ibb.co/YTntpnv/Rectangle-2.png" alt="" className="object-cover object-center w-80  h-80 dark:bg-gray-500" />
+	<div className="flex flex-col justify-between py-6 space-y-8">
+		<div className="space-y-2 text-white">
+            <p className='text-xl tracking-wider'>Weekend Mix</p>
+			<h2 className="text-3xl font-bold tracking-wide">Animals Uproots</h2>
+            <p><span className='text-[#F48C06]'>#edm</span> driving time</p>
+			
+		</div>
+		
+	</div>
+</div>
+            <div className="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
+	<img src="https://i.ibb.co/p1pwZyP/Rectangle-3.png" alt="" className="object-cover object-center w-80  h-80 dark:bg-gray-500" />
+	<div className="flex flex-col justify-between py-6 space-y-8">
+		<div className="space-y-2 text-white">
+            <p className='text-xl tracking-wider'>Travel Mix</p>
+			<h2 className="text-3xl font-bold tracking-wide">High Road</h2>
+            <p><span className='text-[#F48C06]'>#country</span> solo-travel time</p>
+			
+		</div>
+		
+	</div>
+</div>
+            </div>
+
+           <div className='text-center mt-7'> <button className='px-6 border-white border-2 py-3 text-white text-center text-lg  font-light'>VIEW MORE</button></div>
+            
+        </div>
+    );
+};
+
+export default Release;
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from '../Components/Nav/Nav';
 import Banner from '../Components/Banner/Banner';
+import Release from '../Components/Release/Release';
 
 const Home = () => {
     return (
         <div className='max-w-[1920px]'>
             <Nav></Nav>
             <Banner></Banner>
+            <Release></Release>
         </div>
     );
 };
